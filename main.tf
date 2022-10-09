6af9f9288c0586815fc02818a875dbc1279f2470
Uzair Shaikh <uzairmsk.k@gmail.com>
2022-10-09 15:40:15 +0000

    compute instance

--- main.tf
+++ main.tf
@@ -4,18 +4,46 @@ provider "google" {
   zone="us-east1-b"
 }
 
-resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
-  name          = "test-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = "us-central1"
-  network       = google_compute_network.custom-test.id
-  secondary_ip_range {
-    range_name    = "tf-test-secondary-range-update1"
-    ip_cidr_range = "192.168.10.0/24"
-  }
+resource "google_service_account" "default" {
+  account_id   = "service_account_id"
+  display_name = "Service Account"
 }
 
-resource "google_compute_network" "custom-test" {
-  name                    = "test-network"
-  auto_create_subnetworks = false
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
 }
