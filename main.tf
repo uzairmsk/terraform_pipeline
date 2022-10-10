55e8fc154e8a1474514e316b43d4142667602022
Uzair Shaikh <uzairmsk.k@gmail.com>
2022-10-10 05:27:54 +0000

    Adding VM

--- main.tf
+++ main.tf
@@ -20,3 +20,28 @@ resource "google_service_account" "service_account" {
   account_id   = "uzairshaikh02982"
   display_name = "Uzair Account 2"
 }
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-micro"
+  zone         = "us-east1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope andasdasd permissions granted via IAM Roles.
+    email  = "uzair-service-account@agile-polymer-364709.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+}
